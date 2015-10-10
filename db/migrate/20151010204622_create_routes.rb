1e248e747ba64ee2aaae13c8ba9e3f73726af092
Martin Granados <margrangr@gmail.com>
2015-10-10 16:02:23 -0500

    integer for indexes

--- db/migrate/20151010204622_create_routes.rb
+++ db/migrate/20151010204622_create_routes.rb
@@ -4,12 +4,10 @@ class CreateRoutes < ActiveRecord::Migration
       t.string :name
       t.integer :cost
       t.integer :time_in_mins
-      t.references :zone1, index: true
-      t.references :zone2, index: true
+      t.integer :zone1
+      t.integer :zone2
 
       t.timestamps null: false
-    end
-    add_foreign_key :routes, :zone1s
-    add_foreign_key :routes, :zone2s
+
   end
 end
